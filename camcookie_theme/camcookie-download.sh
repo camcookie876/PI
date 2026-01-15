0554460259abd5ca981391273539c5030ed2ecb1
Camcookie <camcookie876@gmail.com>
2026-01-15 23:10:16 +0000

    git commit -m "Fix everything in themes"

--- camcookie_theme/camcookie-download.sh
+++ camcookie_theme/camcookie-download.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-
-echo "Downloading Camcookie Control Center installer..."
-
-wget -O $HOME/camcookie-install.sh \
-https://camcookie876.github.io/PI/camcookie_theme/download/camcookie-install.sh
-
+echo "Downloading Camcookie installer..."
+wget -O $HOME/camcookie-install.sh https://camcookie876.github.io/PI/camcookie_theme/download/camcookie-install.sh
 chmod +x $HOME/camcookie-install.sh
-
-echo "Running installer..."
 $HOME/camcookie-install.sh
